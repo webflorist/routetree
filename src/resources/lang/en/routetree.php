6121d8fdec1a67145183eb3e48bc9556af9ed980
Gerald Buttinger <gerald.buttinger@nic.at>
2016-12-02 17:34:57 +0100

    add english translations

--- src/resources/lang/en/routetree.php
+++ src/resources/lang/en/routetree.php
@@ -2,20 +2,11 @@
 
 return [
 
-    'honeypotFieldLabel' => 'Please leave this field empty!',
-    'honeypotError' => 'Please leave this field empty!',
+    'editTitle' => 'Edit: :item',
+    'editNavTitle' => 'Edit',
+    'editPathSegment' => 'edit',
+    'createTitle' => 'Create: :resource',
+    'createNavTitle' => 'Create',
+    'createPathSegment' => 'create',
 
-    'captchaQuestions' => [
-        'math' => 'Please solve this simple calculation: :calc'
-    ],
-
-    'captchaPlaceholder' => 'Please solve the calculation stated above.',
-    'captchaHelpText' => 'This is required to protect this form against exploitation by bots.',
-    'captchaError' => 'The result is not correct. Please solve the stated calculation.',
-
-    'timeLimitError' => 'This form can only be submitted every :timeLimit seconds. Please wait a while before submitting again.',
-
-    'dynamicListAddButtonTitle' => 'Add :attribute',
-    'dynamicListRemoveButtonTitle' => 'Remove :attribute',
-    'dynamicListMaximumReached' => 'You have reached the maximum item count. You have to remove an item to be able to add a new one.',
 ];
