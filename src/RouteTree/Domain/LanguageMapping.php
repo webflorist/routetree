bd443bee8541613d57fc5ef4de28f12c90944004
Gerald Buttinger <gerald@code.florist>
2019-11-24 21:16:53 +0100

    allow passing of locale => value array as parameter in LanguageMapping::create

--- src/RouteTree/Domain/LanguageMapping.php
+++ src/RouteTree/Domain/LanguageMapping.php
@@ -7,8 +7,14 @@ class LanguageMapping
 
     protected $values=[];
 
-    public static function create() {
-        return new self();
+    public static function create(?array $languageMapping=null) {
+        $instance = new self();
+        if (is_array($languageMapping)) {
+            foreach ($languageMapping as $locale => $value) {
+                $instance->set($locale, $value);
+            }
+        }
+        return $instance;
     }
 
     public function set(string $locale, $value) {
